0b287a2e4a61911b9f710236f84122f5e405c421
yassine-ennatiqi <yassineennat@gmail.com>
2024-02-07 21:02:26 +0100

    add json file

--- bank.py
+++ bank.py
@@ -2,9 +2,6 @@ import random
 import json
 from termspark.termspark import TermSpark
 
-compte={}
-client={}
-clientcompte={}
 
 def result(message, color="white", background="black"):
     termspark = TermSpark()
@@ -35,26 +32,29 @@ def welcome():
 
 def ajouter(numCl,MPC,SoldeC,numC):
     for i in l :
-        if numCl == i['n de client']:
+        if numCl == i['n_de_client']:
             error("ce compte deja existe changer le numero de client")
             return False
+        if numC ==  i['n_de_compte'] : 
+            numC=genererNumCompte(numCl)
+
     termspark = TermSpark()
     termspark.spark_left([ f"le némuro de votre compte est {numC}", 'black', 'magenta'])
     termspark.spark()
-    dic = { "n de client" :numCl ,
-            "n de compte" : numC , 
+    dic = { "n_de_client" :numCl ,
+            "n_de_compte" : numC , 
             "sold" : SoldeC ,
-            "mot de passe" : MPC
+            "mot_de_passe" : MPC
             }
     f = open("file.json" , "w")    
     l.append(dic)
-    f.write(json.dumps(l))
+    f.write(json.dumps(l, indent=4))
     f.close()
 def suprimmer(numCl):
     for i in l :
-        if numCl == i['n de client']:
+        if numCl == i['n_de_client']:
             l.remove(i)
-            numC = i['n de compte']
+            numC = i['n_de_compte']
             f = open("file.json" , "w")    
             f.write(json.dumps(l))
             return numC
@@ -62,9 +62,9 @@ def suprimmer(numCl):
 
 def neveuxMOT(numCl)  :
     for i in l :
-        if numCl == i['n de client']:
-            NMPC=input("entrer le neveux mot de passe: ")
-            i['mot de passe'] = NMPC
+        if numCl == i['n_de_client']:
+            NMPC=input("entrer le neveux mot_de_passe: ")
+            i['mot_de_passe'] = NMPC
             f = open("file.json" , "w")    
             f.write(json.dumps(l))
             return True
@@ -73,33 +73,33 @@ def neveuxMOT(numCl)  :
 
 def affichageSOLDE(numC):
     for i in l :
-        if numC == i['n de compte']:
+        if numC == i['n_de_compte']:
             return i['sold']
     return False
         
 def déposer(numC,soldeDéposer):
     for i in l :
-        if numC == i['n de compte'] and soldeDéposer > 0 :
+        if numC == i['n_de_compte'] and soldeDéposer > 0 :
         
             i['sold'] = i['sold'] + soldeDéposer
             f = open("file.json" , "w")    
             f.write(json.dumps(l))
             success("L'opération s'est terminée avec succès ")
             success(f"votre solde est {i['sold']} ")
-        elif numC != i['n de compte']:
+        elif numC != i['n_de_compte']:
             error("Le compte n'existe pas.")
         elif soldeDéposer <= 0:
             error("solde déposer incorrect")
 
 def retirer(numC,soldeRetirer):
     for i in l :
-        if numC == i['n de compte']and soldeRetirer <= i['sold'] and soldeRetirer > 0 :
+        if numC == i['n_de_compte']and soldeRetirer <= i['sold'] and soldeRetirer > 0 :
             i['sold'] = i['sold'] - soldeRetirer
             f = open("file.json" , "w")    
             f.write(json.dumps(l))
             success("L'opération s'est terminée avec succès ")
             success(f"votre solde est {i['sold']} ")
-        elif numC != i['n de compte']:
+        elif numC != i['n_de_compte']:
             error("Le compte n'existe pas.")
         elif soldeRetirer > i['sold'] or soldeRetirer <= 0:
             error("solde retirer incorrect")
@@ -109,11 +109,13 @@ genererNumCompte = lambda numCl: int(str(numCl) + str(random.randint(0, 100)))
 welcome()
 while True : 
     f = open("file.json" , "r")
-    l = json.loads(f.read() or "[]")  
+    l = json.loads(f.read() or "[]")
+
     choix("1.menu agent")
     choix("2.menu client")
     choix("3.Quitter")
-    choix1=input("Choisissez une option (1, 2 ou 3): ")      
+    choix1=input("Choisissez une option (1, 2 ou 3): ")   
+       
     if choix1 == "1":
 
         choix("1.ajouter un compte")
@@ -122,7 +124,7 @@ while True :
         choix2=input("Choisissez une option (1, 2 ou 3): ")
         if choix2=="1":
             numCl=int(input("entrer le numéro de client: "))
-            MPC=input("entrer le mot de passe: ")
+            MPC=input("entrer le mot_de_passe: ")
             numC=genererNumCompte(numCl)
             SoldeC=float(input("entrer le sold: "))
             ajouter(numCl,MPC,SoldeC,numC)
@@ -135,7 +137,7 @@ while True :
         if choix2=="3":
             continue
     if choix1=="2":
-        choix("1.Modifier son mot de passe")
+        choix("1.Modifier son mot_de_passe")
         choix("2.Afficher son solde")
         choix("3.Déposer une somme d’argent")
         choix("4.Retirer une somme d’argent")
@@ -144,7 +146,7 @@ while True :
         if choix3=="1":
             numCl=int(input("entrer le numéro de client: "))
             if neveuxMOT(numCl) :
-                success("Le mot de passe a été modifié avec succès.")
+                success("Le mot_de_passe a été modifié avec succès.")
             else:
                 error("Le compte n'existe pas.")
         if choix3=="2":
@@ -165,8 +167,4 @@ while True :
             continue
     if choix1=="3":
             exit()        
-# a = {12 : 12}
-# if 12 in a :
-#     print("yyyyyyyyyyyyyyyyes")
-# else :
-#     print("nnnnnnnnnnnnon")    
+  
